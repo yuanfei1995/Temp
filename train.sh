2ce61a4b090cb49d2b03a4a7e5339e45917a6373
YuAN FeI <39902856+yuanfei1995@users.noreply.github.com>
2022-12-14 08:24:01 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,4 @@
+python train.py --level 0 --modelpath 'models/level0' --logpath 'logs/level0' 
+python train.py --level 1 --modelpath 'models/level1' --logpath 'logs/level1' 
+python train.py --level 2 --modelpath 'models/level2' --logpath 'logs/level2' 
+python train.py --level 3 --modelpath 'models/level3' --logpath 'logs/level3' 
